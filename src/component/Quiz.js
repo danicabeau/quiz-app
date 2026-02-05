f7d844371f4a5098db1782f08b37c97f3aedd4c3
danicabeau <126159395+danicabeau@users.noreply.github.com>
2026-02-05 10:08:41 +0700

    add checkAnswer

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,17 +1,34 @@
 import QuestionData from "../data/QuestionData";
+import { useEffect, useState } from "react";
 
 const Quiz = () => {
     //console.log(QuestionData);
     const [current,setCursor] = useState(0);
+    const [selectChoice,setSelectChoice] = useState("");
+
+    useEffect(()=>{
+        checkAnswer()
+    },[selectChoice])
+
+    const checkAnswer = () => {
+        if(selectChoice !== ""){
+            if(selectChoice === QuestionData[current].answer){
+                console.log("ตอบถูกและได้คะแนน");
+            }else{
+                console.log("ตอบผิดไม่ได้คะแนน T+T");
+        }
+    }
+
     return(
         <div className="quiz" >
             <h1>{QuestionData[current].question}</h1>
             <div className="choices">
-                <button>{QuestionData[current].A}</button>
-                <button>{QuestionData[current].B}</button>
-                <button>{QuestionData[current].C}</button>
-                <button>{QuestionData[current].D}</button>
+                <button onClick={()=>setSelectChoice("A")}>{QuestionData[current].A}</button>
+                <button onClick={()=>setSelectChoice("B")}>{QuestionData[current].B}</button>
+                <button onClick={()=>setSelectChoice("C")}>{QuestionData[current].C}</button>
+                <button onClick={()=>setSelectChoice("D")}>{QuestionData[current].D}</button>
             </div>
+            <p>{'{current+1} / {QuestionData.length}'}</p>
         </div>
         
     )

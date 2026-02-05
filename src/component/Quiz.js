9886721cd51f6507c2a17d6354309a8c55276055
danicabeau <126159395+danicabeau@users.noreply.github.com>
2026-02-05 10:00:30 +0700

    Update Quiz.js

--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,7 +1,17 @@
+import QuestionData from "../data/QuestionData";
+
 const Quiz = () => {
+    //console.log(QuestionData);
+    const [current,setCursor] = useState(0);
     return(
         <div className="quiz" >
-            <h1>Quiz Component</h1>
+            <h1>{QuestionData[current].question}</h1>
+            <div className="choices">
+                <button>{QuestionData[current].A}</button>
+                <button>{QuestionData[current].B}</button>
+                <button>{QuestionData[current].C}</button>
+                <button>{QuestionData[current].D}</button>
+            </div>
         </div>
         
     )
